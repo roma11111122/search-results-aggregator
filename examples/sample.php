cb764b0bae9e8c370e41570d3a3488ab7c45ab41
tdmytrykiv <t.dmytrykiv@gmail.com>
2018-11-02 14:25:24 +0200

    Update sample.php

--- examples/sample.php
+++ examples/sample.php
@@ -3,7 +3,9 @@ require_once '../vendor/autoload.php';
 
 use \Roma11111122\SearchResultAggregator\SearchAggregator;
 
-
+//why search keyword in constructor 
+//what will be if I whant to search several time
 $aggregator = new SearchAggregator('python');
+//why not to use keyword here getAggregatedData(string $keyword)
 $result = $aggregator->getAggregatedData();
 echo var_dump($result);
